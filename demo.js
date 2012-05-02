976894a6be3b09fd0267c1c6db4ab10f1cfd9e88
iamlaobie <iamlaobie@gmail.com>
2012-05-03 00:41:36 +0800

    dev

--- demo.js
+++ demo.js
@@ -1,8 +1,7 @@
 var ictclas = require('./build/Release/ictclas');
 var s = "反省中：在餐馆吃饭，等米饭太久，失去耐心，对服务员大吼，瞬间变成我鄙视的人";
 ictclas.init('/opt/ictcla/Sample/Linux_64_C_Demo');
-//ictclas.importDictFile("/ws/ictclas/userdict.txt");
-ictclas.importDictWords("反省中;失去耐心;等米饭太久;");
+ictclas.importDictFile("/ws/ictclas/userdict.txt");
 ictclas.segment(s, function(msg){
 	console.log(msg);
 });
